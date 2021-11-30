b52d531aaffca8c9e78db736ae61f022d592bff9
Gaoussou Diallo <gaoussou1993@gmail.com>
2021-11-30 12:32:10 -0800

    first commit.house cleaning installing dependencies for dev

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Theboyg-Rest-API-with-express-and-MongoDB
+This will serve as the backend application for Social-Media-Platform
---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("backend server is running bnkdwbvcsin ");
+});
